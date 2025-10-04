136e341b0acb3377bc8f94c2f0c3a531fca71610
Hichem Abderrahmane Ali Mahdi <hali-mah@3-I-4.42heilbronn.de>
2025-10-04 16:13:45 +0200

    simple first script part with switch case struct

--- src/script.js
+++ src/script.js
@@ -1,19 +1,13 @@
-let wordTyped = "guess" 
-let userWord = prompt("Enter a word:")
-
-switch (userWord){
-    case wordTyped:
-        console.log("Congratulations! you have typed the correct word.\n")
-        break
-    case "idiot":
-        console.log("Be polite!")
-        break
-    case "dumb":
-        console.log("Be polite!")
-        break
-    case "vilain":
-        console.log("Be polite!")
+const wordList = ["Cachalot", "Lily", "Minnie", "Various", "Towel"]
+let score = 0
+let wordToType = prompt("You have this list:\n" + wordList.join(", ") + "\nguess the word that I am thinking:") 
+switch (wordToType){
+    case wordList[0]:
+        console.log("Congratulations! you guessed the correct word\n")
         break
     default:
-        console.log("You made a typo error.")
+        if (!wordList.includes(wordToType))
+            console.log("The word you typed is not in the list\n")
+        else
+            console.log("You made a mistake\n")
 }
