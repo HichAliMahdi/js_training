82ba208fa6fc729a7323227dc196700cb58d7fac
Hichem Abderrahmane Ali Mahdi <hali-mah@3-I-4.42heilbronn.de>
2025-10-04 15:54:28 +0200

    tried simple if else logic

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
-console.log("Hello world")
+let wordTyped = "guess" 
+let userWord = prompt("Enter a word:")
+
+if (wordTyped === userWord) 
+    console.log("Congratulations! you have typed the correct word.\n")
+else
+    console.log("Wrong! Better luck next time.\n")
+
+console.log(userWord)
