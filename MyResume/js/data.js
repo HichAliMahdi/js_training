24e12b67678e160b4fa035bb6b61af854610576a
Hichem Abderrahmane Ali Mahdi <hali-mah@2-B-2.42heilbronn.de>
2025-10-14 18:14:45 +0200

    Updating my data to my resume variable

--- MyResume/js/data.js
+++ MyResume/js/data.js
@@ -6,5 +6,34 @@ const ResumeData = {
         country: "Germany",
         email: "hichem.alimahdi@gmail.com",
         phNbr: "+49 163 241 0341"
-    }
+    },
+    experiece: [
+        {
+            company: "DJEZZY OTA",
+            location: "Algiers",
+            country: "Algeria",
+            period: "June 2024 - Sep 2024",
+            responsibilities: [
+                "Resolved Level 1 to Level 3 incidents, reducing average response time by 30%.",
+                "Monitored and maintained cloud services with high availability targets.",
+                "Documented intervention reports and contributed to technical procedures.",
+                "Identified platform anomalies, supported mitigation strategies.",
+                "Conducted updates, supervised platform performance, and improved the cloud knowledge base."
+            ]
+        },
+        {
+            company: "SAARL DATAGIX",
+            location: "Algiers",
+            country: "Algeria",
+            period: "Jul 2020 - Mar 2024",
+            responsibilities : [
+                "Managed Windows and Linux environments and implemented L1/L2 virtualization.",
+                "Oversaw software installation, updates, and patch management.",
+                "Configured routers, switches, and firewalls, ensuring secure network connectivity.",
+                "Deployed backup solutions and disaster recovery plans.",
+                "Delivered user support and created technical documentation.",
+                "Maintained 99%+ system uptime across multiple client environments."
+            ]
+        },
+    ]
 }
